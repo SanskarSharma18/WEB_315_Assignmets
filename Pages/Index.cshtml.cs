e4141e576d4cb3ea110dc57a63f5559d642d8614
SanskarSharma18 <136133385+SanskarSharma18@users.noreply.github.com>
2023-06-28 18:29:16 -0400

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,1 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace SanskarEcoQuest.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public List<Destinations> Destinations { get; set; }
+
+}
