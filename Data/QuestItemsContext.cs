7ee264e269c78d5ee95a0b264092685594385699
SanskarSharma18 <136133385+SanskarSharma18@users.noreply.github.com>
2023-06-28 18:32:01 -0400

    Update QuestItemsContext.cs

--- Data/QuestItemsContext.cs
+++ Data/QuestItemsContext.cs
@@ -1,1 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using QuestItems.Models;
 
+// create databse context class
+
+namespace QuestItems.Data
+{
+    public class QuestItemsContext : DbContext
+    {
+        public QuestItemsContext(DbContextOptions<QuestItemsContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<QuestItems.Models.QuestItems> QuestItems { get; set; }
+    }
+}
